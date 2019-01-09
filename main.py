b21bba514878f0706915cf7dc4b5227b0c378627
Stephen Merity <smerity@smerity.com>
2019-01-09 22:24:50 +1100

    After button replies sending a tutor

--- main.py
+++ main.py
@@ -74,9 +74,26 @@ def slash_smerlock():
 @app.route('/slack/action-endpoint', methods=['POST'])
 def action_endpoint():
   print('Action received:', flask.request.values)
+  # Prints (if responding to the smerlock):
+  # ImmutableMultiDict([('payload', '{"type":"interactive_message","actions":[{"name":"location","type":"button","value":"womens"}],"callback_id":"lockout_location","team":{"id":"TEKDUUF5G","domain":"ncss-2019-tutors"},"channel":{"id":"CFAULCTD4","name":"smerbot"},"user":{"id":"UF4GJ5Y9K","name":"smerity"},"action_ts":"1547031403.733548","message_ts":"1547031400.000200","attachment_id":"1","token":"FwydmeYg2VrS4vToSryWBkEK","is_app_unfurl":false,"original_message":{"type":"message","subtype":"bot_message","text":"Where are you locked out?","ts":"1547031400.000200","bot_id":"BF90RSZ17","attachments":[{"callback_id":"lockout_location","fallback":"If you can\'t select a location the helper will message you","text":"Choose a location","id":1,"actions":[{"id":"1","name":"location","text":"Main Building","type":"button","value":"main","style":""},{"id":"2","name":"location","text":"ABS","type":"button","value":"abs","style":""},{"id":"3","name":"location","text":"Women\'s College","type":"button","value":"womens","style":""},{"id":"4","name":"location","text":"SIT","type":"button","value":"sit","style":""}]}]},"response_url":"https:\\/\\/hooks.slack.com\\/actions\\/TEKDUUF5G\\/519975990261\\/QguWZ3Z2g7wM3mBe3Tbf6SPT","trigger_id":"519774931746.495470967186.c587a5aa20bd8fd19c692cce34ca20c3"}')])])
+  # Less horrific version of this response is at https://api.slack.com/interactive-messages > Checking the action type
+  ###
   # Your response must be a 200
   # Responding immediately with JSON will replace the current message
   # You can create a new message by setting replace_original to false
+  ###
+  # We'll assume there's only a single action
+  # Honestly I don't know if there is anything where it replies with multiple
+  # If it does I don't know how you respond with JSON
+  payload = flask.request.values['payload']
+  callback = payload['callback_id']
+  if callback == 'lockout_location':
+    actions = {}
+    for action in payload.get('actions', []):
+      name = action['name']
+      value = action['value']
+      actions[name] = valu
+    return f"Sending tutor to {actions['location']}"
   return ""
 
 if __name__ == '__main__':
