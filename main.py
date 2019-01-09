64ac9f4e53a693504c5bb57427f1189518e88079
Stephen Merity <smerity@smerity.com>
2019-01-09 17:08:20 +1100

    Return a text response

--- main.py
+++ main.py
@@ -10,6 +10,11 @@ def hello_world():
 @app.route('/slack/slash/zah', methods=['POST'])
 def slash_zah():
   print(flask.request.form)
+  # https://api.slack.com/slash-commands
+  # -> Sending an immediate response
+  # You can reply with plain text
+  return '... butzah!'
+  # or with a JSON response
 
 if __name__ == '__main__':
   app.run()
