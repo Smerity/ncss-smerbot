329941d0481757bbb2f317ce0f9c5e6c297677fa
Stephen Merity <smerity@smerity.com>
2019-01-09 17:00:28 +1100

    Add zah command

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+state = []
+
 @app.route('/')
 def hello_world():
-  return 'Hello World!'
+  return 'Hello World v0.1'
+
+@app.route('/slack/slash/zah')
+def slash_zah():
+  print(request.form)
 
 if __name__ == '__main__':
   app.run()
