0b0eb8f40301c34d12548abc0a9b2eaf5893c06c
Stephen Merity <smerity@smerity.com>
2019-01-09 17:11:45 +1100

    Zah butzah with full response

--- main.py
+++ main.py
@@ -9,12 +9,15 @@ def hello_world():
 
 @app.route('/slack/slash/zah', methods=['POST'])
 def slash_zah():
+  # If you print something out it'll appear in
+  # https://dashboard.heroku.com/apps/smerbot/logs
   print(flask.request.form)
-  # https://api.slack.com/slash-commands
-  # -> Sending an immediate response
+  # For https://api.slack.com/slash-commands "Sending an immediate response"
   # You can reply with plain text
   return '... butzah!'
   # or with a JSON response
+  attachments = [{'text': 'Zah butzah is a game from when NCSS ran on Z80 CPUs'}]
+  return flask.jsonify({'text': '... butzah!', 'attachments': attachments})
 
 if __name__ == '__main__':
   app.run()
